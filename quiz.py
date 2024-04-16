22a12e1df401b1d7c657104a35da1187ace7daf0
LincolnC2008 <lcrowell637@gmail.com>
2024-04-15 19:00:03 -0700

    Add questions list and addQuestion()

--- quiz.py
+++ quiz.py
@@ -1,1 +1,6 @@
-questions = ["will add later"]
+questions = []
+
+def addQuestion(question, answersAr, correctAnswer):
+    questions.push({"question":question, "answers":answersAr, "correctAnswer":correctAnswer})
+    
+addQuestion()
