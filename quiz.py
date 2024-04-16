6e3a5c9e06f30be728f73d9bf4ddea54c7fc00a1
LincolnC2008 <lcrowell637@gmail.com>
2024-04-15 19:13:13 -0700

    Add answerIsCorrect() and score variable

--- quiz.py
+++ quiz.py
@@ -1,6 +1,14 @@
 questions = []
+userAnswer = None
+score = 0
 
 def addQuestion(question, answersAr, correctAnswer):
     questions.push({"question":question, "answers":answersAr, "correctAnswer":correctAnswer})
     
 addQuestion()
+
+def answerIsCorrect(correctAnswer):
+    if correctAnswer == userAnswer:
+        return True
+    else:
+        return False
