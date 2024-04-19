cdcd2bd01a408b898d579a36a9265367f2194dfc
LincolnC2008 <lcrowell637@gmail.com>
2024-04-19 16:21:05 -0700

    Add getUserChoice, and fix addQuestion

--- quiz.py
+++ quiz.py
@@ -3,12 +3,18 @@ userAnswer = None
 score = 0
 
 def addQuestion(question, answersAr, correctAnswer):
-    questions.push({"question":question, "answers":answersAr, "correctAnswer":correctAnswer})
+    questions.append({"question":question, "answers":answersAr, "correctAnswer":correctAnswer})
     
-addQuestion()
 
 def answerIsCorrect(correctAnswer):
     if correctAnswer == userAnswer:
         return True
     else:
         return False
+    
+def getUserChoice():
+    userAnswer = input("Please select either a, b, or c")
+    
+addQuestion("What is Clash Royale?", ["a: A game", "b: A book", "c: A car"], "a: A game")
+
+print(questions)
