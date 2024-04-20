82f175e9ca940850b73c93cf929bde1c50d1fc55
LincolnC2008 <lcrowell637@gmail.com>
2024-04-19 17:33:04 -0700

    Fix getUserChoice logic

--- quiz.py
+++ quiz.py
@@ -1,28 +1,37 @@
 questions = []
-userAnswer = None
+# userAnswer = None
 score = 0
-currentQuestion = None
+# currentQuestion = None
+
+
 
 def addQuestion(question, answersAr, correctAnswer):
-    questions.append({"question":question, "answers":answersAr, "correctAnswer":correctAnswer})
-    
+    questions.append({"question":question, "answers":answersAr, "correctAnswer":correctAnswer, "userAnswer":""})
+
 
 def answerIsCorrect(correctAnswer):
     if correctAnswer == userAnswer:
         return True
     else:
         return False
-    
-def getUserChoice():
-    userAnswer = input("Please select either a, b, or c")
-    
+
+def getUserChoice(currentQuestion):
+    # userAnswer = input("Please select either a, b, or c")
+    currentQuestion["userAnswer"] = input("Please select either a, b, or c: ")
+
+
 def displayQuestion(questionIndex):
     currentQuestion = questions[questionIndex]
     print(currentQuestion["question"])
     for i in range(len(currentQuestion["answers"])):
         print(currentQuestion["answers"][i])
     print()
-            
-addQuestion("What is Clash Royale?", ["a: A game", "b: A book", "c: A car"], "a: A game")
+
+addQuestion("What is Clash Royale?", ["a: A game", "b: A book", "c: A car"], "a")
 
 displayQuestion(0)
+getUserChoice(questions[0])
+
+
+
+print()
